4ac2f25ab0c10fec52772addf9a599dbefbca0b2
xvxd4sh <alexongkowijoyo91@ymail.com>
2022-08-16 09:57:23 -0400

    tests and linter fix

--- tests/container_test.py
+++ tests/container_test.py
@@ -18,12 +18,12 @@ RELEASE_TAG = os.getenv("RELEASE_TAG")
 VERSION_FILE = "src/version.txt"
 
 
-# def test_container_count(dockerc):
-#    """Verify the test composition and container."""
-#    # stopped parameter allows non-running containers in results
-#    assert (
-#        len(dockerc.containers(stopped=True)) == 1
-#    ), "Wrong number of containers were started."
+ def test_container_count(dockerc):
+    """Verify the test composition and container."""
+    # stopped parameter allows non-running containers in results
+    assert (
+        len(dockerc.containers(stopped=True)) == 1
+    ), "Wrong number of containers were started."
 
 
 # def test_wait_for_ready(main_container):
@@ -57,7 +57,7 @@ VERSION_FILE = "src/version.txt"
 # @pytest.mark.skipif(
 #    RELEASE_TAG in [None, ""], reason="this is not a release (RELEASE_TAG not set)"
 # )
-#def test_release_version():
+# def test_release_version():
 #    """Verify that release tag version agrees with the module version."""
 #    pkg_vars = {}
 #    with open(VERSION_FILE) as f:
